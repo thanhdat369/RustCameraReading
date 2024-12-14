6116e479345c3e5cd61cc2131df96329bb263cde
Dat Le <dat.lethanh369@gmail.com>
2024-12-14 23:25:15 +0700

    Testing with nokhwa

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 - [ ] Read the image from the webcam
 - [ ] Show the image
 - [ ] Send to server
+
+### References Link
+https://github.com/security-union/av1-streaming/tree/master
+https://github.com/l1npengtul/nokhwa/tree/senpai
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use nokhwa::pixel_format::RgbFormat;
+use nokhwa::utils::{CameraIndex, RequestedFormat, RequestedFormatType};
+use nokhwa::Camera;
+
 fn main() {
-    println!("Hello, world!");
+    let index = CameraIndex::Index(1);
+
+    // Request the highest resolution CameraFormat that can be decoded to RGB
+    let requested =
+        RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+
+    // Try to initialize the camera
+    let camera = Camera::new(index, requested);
+
+    match camera {
+        Ok(mut camera) => {
+            // Try to capture a frame
+            let frame = camera.frame();
+
+            match frame {
+                Ok(frame) => {
+                    println!("Captured Single Frame of {}", frame.buffer().len());
+
+                    // Try to decode the frame into an ImageBuffer
+                    let decoded = frame.decode_image::<RgbFormat>();
+
+                    match decoded {
+                        Ok(decoded_image) => {
+                            println!("Decoded Frame of {}", decoded_image.len());
+                        }
+                        Err(e) => {
+                            eprintln!("Error decoding frame: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error capturing frame: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Error initializing camera: {}", e);
+        }
+    }
 }
